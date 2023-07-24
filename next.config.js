6b4e9577bd82ffb5c0d22d70f663a4cd2ff717cd
yoodeve <janhold@naver.com>
2023-07-24 10:55:32 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/gh-pages-test",
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
